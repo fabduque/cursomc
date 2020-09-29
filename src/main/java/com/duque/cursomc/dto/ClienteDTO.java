6b19c1be947e60bf6229296b041355373a282380
fabduque <fdef1975@gmail.com>
2020-09-29 19:38:50 -0300

    Validação customizada: Email não repetido na Atualização de CLiente

--- src/main/java/com/duque/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/duque/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.duque.cursomc.domain.Cliente;
+import com.duque.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
